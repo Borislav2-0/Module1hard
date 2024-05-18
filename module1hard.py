94d6f7b7692327eb4dd92908a3613fdbd3bd1b12
Borislav2-0 <crva20@mail.ru>
2024-05-18 09:45:25 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,9 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+grades_ = 0
+students_1 = sorted(students)
+students_2 = 0
+for i in grades:
+    print(students_1[students_2], ((sum(grades[grades_]) / len(grades[grades_]))))
+    grades_ += 1
+    students_2 += 1
